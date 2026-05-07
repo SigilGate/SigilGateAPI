1131f97071cf231d1e7b2268ffa25a050ee29279
SigilGate <open.sigil.gate@gmail.com>
2026-05-07 15:18:22 +0100

    Create fake_random.go

---
+++ internal/adapter/fake_random.go
@@ -0,0 +1,16 @@
+package fake_random
+
+import "fmt"
+
+type FakeRandom struct {
+	counter int
+}
+
+func NewFakeRandom() *FakeRandom {
+	return &FakeRandom
+}
+
+func (r *FakeRandom) UUID() string {
+	r.counter++
+	return fmt.Sprintf("00000000-0000-0000-0000-%012d", r.counter)
+}
