e30ecb7ab2d96811172d1307e0212e648e46edb8
SigilGate <open.sigil.gate@gmail.com>
2026-05-08 02:17:07 +0100

    Update fake_clock.go

--- internal/adapter/fake_clock.go
+++ internal/adapter/fake_clock.go
@@ -1,10 +1,14 @@
 package adapter
 
-import "time"
+import (
+	    "time"
+		"github.com/SigilGate/SigilGateAPI/internal/port")
 
 type FakeClock struct {
 	fixed time.Time
 }
+
+var _ port.ClockPort = (*FakeClock)(nil)
 
 func NewFakeClock(t time.Time) *FakeClock {
 	return &FakeClock{fixed: t}
