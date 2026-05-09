b21058b1d945817732863184e51ac47f7f5ff2fe
SigilGate <open.sigil.gate@gmail.com>
2026-05-09 17:57:43 +0100

    Create user.go

---
+++ internal/store/record/user.go
@@ -0,0 +1,10 @@
+package record
+
+type UserRecord struct {
+    ID             string
+    Username       string
+    Status         string
+    StatusAt       string
+    HashTelegramID string
+    Created        string
+}
